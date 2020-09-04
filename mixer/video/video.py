7d88d418840f22fda360cd544b0f265e5ef74c6a
Luis Fernando Cruz Carrillo <lfcruz@udes.edu.mx>
2020-09-04 17:32:19 -0500

    Avances

--- mixer/video/video.py
+++ mixer/video/video.py
@@ -4,6 +4,21 @@ import subprocess
 from mixer.file_manager import GeneralConf, VFiles
 
 
+class Processor(object):
+    """ Class for process de video streams. """
+
+    default_scale = "1920x1080"
+
+    def get_scale(self, video):
+        command = ["ffprobe", "-v", "error",
+                   "-select_streams", "v:0",
+                   "-show_entries", "stream=width,height",
+                   "-of", "csv=s=x:p=0", video]
+        return (subprocess
+                .run(command, capture_output=subprocess.PIPE)
+                .stdout.decode('utf-8'))
+
+
 def video_processor(conf_file):
     """ Process videos. """
     general_conf = GeneralConf.retrive_file()
@@ -15,7 +30,10 @@ def video_processor(conf_file):
             general_key = data["video"][1:]
             print(general_key)
         else:
-            command = ["ffprobe", "-v", "error", "-select_streams", "v:0", "-show_entries", "stream=width,height", "-of", "csv=s=x:p=0", data['video']]
-            scale = subprocess.run(command, capture_output=subprocess.PIPE)
-            print(scale.stdout.decode('utf-8'))
+            command = ["ffprobe", "-v", "error", "-select_streams", "v:0",
+                       "-show_entries", "stream=width,height", "-of", "csv=s=x:p=0", data['video']]
+            scale = (subprocess
+                     .run(command, capture_output=subprocess.PIPE)
+                     .stdout.decode('utf-8'))
 
+            print(scale)
